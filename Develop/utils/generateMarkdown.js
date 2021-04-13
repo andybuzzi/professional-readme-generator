7b231003c4634ae377236631d8a4a69e5e596b3a
andersonbuzzidr <77116414+andersonbuzzidr@users.noreply.github.com>
2021-04-13 17:57:40 -0500

    folder fixed

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,116 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function to generate contribution
+const generateContribute = (contributeText) => {
+  if (!contributeText) {
+    return "";
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  return `
+## Contributing
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+${contributeText};
+    `;
+};
 
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+//function to generate tests
 
+const generateTests = (testsText) => {
+  if (!testsText) {
+    return "";
+  }
+
+  return `
+## Tests
+
+${testsText};
+    `;
+};
+
+//function to generate instructions
+const generateInstructions = (instructionText) => {
+  if (!instructionText) {
+    return "";
+  }
+
+  return `
+### Instructions on how to reach out
+
+${instructionText}
+    `;
+};
+
+// function to add contribution to content table
+const generateContributeTable = (contributeTable) => {
+  if (!contributeTable) {
+    return "";
+  }
+
+  return `
+* [Contributing](#contributing)
+    `;
+};
+
+// function to add tests to content table
+const generateTestsTable = (testsTable) => {
+  if (!testsTable) {
+    return "";
+  }
+
+  return `
+* [Tests](#tests)
+    `;
+};
+
+module.exports = (templateData) => {
+  console.log(templateData);
+
+  // destructure projects and about data from templateData based on their property key names
+
+  return `
+# ${templateData.name}  
+![badge](https://img.shields.io/badge/license-${
+    templateData.license
+  }-brightgreen)
+
+## Description 
+
+${templateData.description}
+
+[Click here to see the deployed project](${templateData.githubPage})
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+${generateContributeTable(templateData.contribute)}
+${generateTestsTable(templateData.tests)}
+* [Questions](#questions)
+
+
+## Installation
+
+${templateData.installation}
+
+
+## Usage 
+
+${templateData.usage}
+
+## License
+
+This project is covered under ${templateData.license} license.
+
+${generateContribute(templateData.contribute)}
+${generateTests(templateData.tests)}
+
+## Questions
+
+Please feel free to contact me if you have any question or need any further information.
+
+* [GitHub Profile](${templateData.username})
+
+* [Email Address](${templateData.email})
+
+${generateInstructions(templateData.reachOut)}
 `;
-}
-
-module.exports = generateMarkdown;
+};
--- README.md
+++ README.md
@@ -1,63 +1,48 @@
+# Anderson’s Portfolio
 
-# a  
 ![badge](https://img.shields.io/badge/license-MIT-brightgreen)
 
-## Description 
+## Description
 
-asd
+Hi there! I'm Anderson and this is my portfolio, here you will find more information about me as well as samples of my work. I've used HTML and CSS for this first step. In the next weeks, I'll be improving my codebase and adding some more languages programming to it. If you have any questions or suggestions on any of my works, feel free to contact me!
 
-[Click here to see the deployed project](a)
-
+[Click here to see the deployed project](https://andybuzzi.github.io/)
 
 ## Table of Contents
 
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
 
-* [Installation](#installation)
-* [Usage](#usage)
-* [License](#license)
+- [Contributing](#contributing)
 
-* [Contributing](#contributing)
-    
-
-* [Tests](#tests)
-    
-* [Questions](#questions)
-
+- [Tests](#tests)
+- [Questions](#questions)
 
 ## Installation
 
-asd
+There’s no instructions for installation at the moment.
 
+## Usage
 
-## Usage 
-
-sadasdasd
+his is a personal portfolio being used to showcase my work
 
 ## License
 
 This project is covered under MIT license.
 
-
 ## Contributing
 
-dsfsadf;
-    
+If you’d like to contribute with any of the projects showcased in this portfolio, please email me with the subject ‘contribute’ and I will get back to you as soon as possible.;
 
 ## Tests
 
-afsdfsdaf;
-    
+Some of the projects may contain tests for the applications, please check them out through the portfolio work section.;
 
 ## Questions
 
 Please feel free to contact me if you have any question or need any further information.
 
-* [GitHub Profile](asdf)
+- [GitHub Profile](andybuzzi.github.com)
 
-* [Email Address](sadf)
-
-
-### Instructions on how to reach out
-
-sadfasfd
-    
+- [Email Address]()
